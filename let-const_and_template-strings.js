f1309d530da16b887ee4568d4f4607eba25b89a7
my first github repo <mrarifmondol98@gmail.com>
2026-08-05 18:50:45 +0600

    es6 problem solving

---
+++ let-const_and_template-strings.js
@@ -0,0 +1,22 @@
+/*
+  Problem 1: Fix the Scope Bug
+  Rewrite using let to fix the output.
+*/
+
+let status = "Order Placed";
+
+for (let i = 1; i <= 3; i++) {
+  let status = "Processing Item " + i;
+  // console.log(status);
+}
+
+// console.log("Final Status:", status);
+
+
+/*
+  Problem 2: Receipt Generator (Template Strings)
+  Build a receipt generator using template strings — take item name,
+  price, qty as input, output formatted multiline receipt.
+
+  Example: Input ("Pen", 20, 3) -> Output "Pen x3 = 60 Taka"
+*/
